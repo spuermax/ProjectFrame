46e1387f084b7968a4941287745ea34a75273431
Super 含 <zihanyh@gmail.com>
2020-03-22 10:24:53 +0800

    页面刷新 。

--- app/src/main/java/com/developers/projectframe/module/home/fragment/HomeFragment.java
+++ app/src/main/java/com/developers/projectframe/module/home/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.developers.projectframe.module.home.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,7 +10,14 @@ import android.view.ViewGroup;
 
 import com.developers.projectframe.R;
 import com.developers.projectframe.base.view.NewBaseFragment;
+import com.developers.projectframe.module.home.MainPresenter;
 import com.developers.projectframe.module.home.adapter.HomeItemAdapter;
+import com.developers.projectframe.module.home.bean.AdvisoryBean;
+import com.developers.projectframe.module.home.contract.MainContract;
+import com.developers.projectframe.utils.ToastUtil;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import java.util.ArrayList;
 
@@ -18,12 +26,17 @@ import java.util.ArrayList;
  * @Date 2020/3/21 15:43
  * @Description
  */
-public class HomeFragment extends NewBaseFragment {
+public class HomeFragment extends NewBaseFragment<MainPresenter> implements MainContract.IMainView {
 
     private RecyclerView recyclerView;
     private ArrayList<String> arrayList;
     private HomeItemAdapter adapter;
+    private RefreshLayout refreshLayout;
 
+    @Override
+    protected MainPresenter loadPresenter() {
+        return new MainPresenter(this);
+    }
 
     @Override
     protected View inflaterView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
@@ -34,6 +47,24 @@ public class HomeFragment extends NewBaseFragment {
     protected void initView(View view) {
         super.initView(view);
         recyclerView = view.findViewById(R.id.recycler);
+        refreshLayout = view.findViewById(R.id.refreshLayout);
+
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                mPresenter.getAdvisoryInfo();
+            }
+        });
+
+        refreshLayout.setEnableLoadMore(true);// 打开加载更多
+
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                ToastUtil.shortToast(mActivity,"加载更多");
+                mPresenter.getAdvisoryInfo();
+            }
+        });
     }
 
     @Override
@@ -42,19 +73,25 @@ public class HomeFragment extends NewBaseFragment {
         arrayList = new ArrayList<>();
 
 
-        for (int i = 0; i < 10 ; i ++){
+        for (int i = 0; i < 10; i++) {
             arrayList.add("Super = " + i);
         }
 
         adapter = new HomeItemAdapter(arrayList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
-
-
-
-
-
     }
 
 
+    @Override
+    public void requestData() {
+
+    }
+
+    @Override
+    public void setAdvisoryInfo(AdvisoryBean bean) {
+        refreshLayout.finishRefresh();
+        refreshLayout.finishLoadMore();
+        ToastUtil.shortToast(mActivity,"刷新页面成功");
+    }
 }
